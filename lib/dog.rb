d5d4e55193535a4e75588c98f92dbee136addbc8
Dani Bassett <dani@danithetallgirl.com>
2020-07-15 22:04:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,19 @@
 class Dog
+  
+  attr_accessor :name, :breed
+  
   def initialize(name, breed = "Mutt")
     @name = name
     @breed = breed
   end
  
-  def name
-    @name
-  end
+  #def name
+   # @name
+  #end
   
-  def breed
-    @breed
-  end
+  #def breed
+   # @breed
+  #end
 end
 
 fido = Dog.new("Fido", "Pug")
